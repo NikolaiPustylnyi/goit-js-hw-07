022bd009673f4b0077b443b047ed69b2e26c5fc9
NikolaiPustylnyi <pnv05081989@gmail.com>
2022-07-19 23:45:30 +0300

    finish 7 hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryItemsList = galleryItems.map(({preview, original, description}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+}).join(''); 
+
+gallery.insertAdjacentHTML('afterbegin', galleryItemsList);
+
+gallery.addEventListener("click", openOriginalImg);
+
+
+function openOriginalImg(event) {
+event.preventDefault();
+
+if (event.target.classList.contains('gallery__image')) {
+  const instance = basicLightbox.create(`
+  <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+}
+}
+
+
+
